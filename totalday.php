d8e4f5cab25ac5e4f171e6a2663d0270536b604a
edwardcwh11039 <edwardchong11039@gmail.com>
2019-10-27 22:53:30 +1100

    report page (all sales made, percentaage of items sold)
    Report page on all sales made (totalday.php), Report page on percentage of items sold (totalprodudct.php)

---
+++ totalday.php
@@ -0,0 +1,69 @@
+<?php
+$host = "localhost";
+    $user = "prasanga_1";
+    $password = "Leon2012";
+    $sql_db = "prasanga_dp2";
+
+$conn = @mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $sql_db
+);
+if(!$conn){
+    echo"<p>Database connection failure</p>";
+}
+
+$query = "SELECT order_date, SUM(total) as day_total from sales group by order_date";
+$result = mysqli_query($conn, $query);
+$counter = 0;
+$dataPoints = array();
+while($row = mysqli_fetch_assoc($result)){
+    $dataPoints[$counter] = array("y" => $row[day_total], "label" => $row[order_date]);
+    $counter++;
+}
+
+/*
+$dataPoints = array(
+	array("y" => 25, "label" => "Sunday"),
+	array("y" => 15, "label" => "Monday"),
+	array("y" => 25, "label" => "Tuesday"),
+	array("y" => 5, "label" => "Wednesday"),
+	array("y" => 10, "label" => "Thursday"),
+	array("y" => 0, "label" => "Friday"),
+	array("y" => 20, "label" => "Saturday")
+);
+*/
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	title: {
+		text: "Revenue-Date Report"
+	},
+	axisY: {
+		title: "Revenue"
+	},
+	axisX: {
+		title: "Date"
+	},
+	data: [{
+		type: "line",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
---
+++ totalproduct.php
@@ -0,0 +1,61 @@
+<?php
+$host = "localhost";
+    $user = "prasanga_1";
+    $password = "Leon2012";
+    $sql_db = "prasanga_dp2";
+
+$conn = @mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $sql_db
+);
+if(!$conn){
+    echo"<p>Database connection failure</p>";
+}
+
+$query = "SELECT name, SUM(total) as product_total from sales s join item i on s.product_id = i.item_id group by product_id";
+$result = mysqli_query($conn, $query);
+$counter = 0;
+$dataPoints = array();
+while($row = mysqli_fetch_assoc($result)){
+    $dataPoints[$counter] = array("label" => $row[name], "y" => $row[product_total]);
+    $counter++;
+}
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	title:{
+		text: "Revenue by Product Sales"
+	},
+	subtitles: [{
+		text: "Currency: Australian Dollar (AUD)"
+	}],
+	data: [{
+		type: "pie",
+		showInLegend: "true",
+		legendText: "{label}",
+		indexLabelFontSize: 16,
+		indexLabel: "{label} - #percent%",
+		yValueFormatString: "AUD#,##0",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
